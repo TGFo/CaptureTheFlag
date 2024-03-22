08686f63e32792df6179329ab24ae2bf81fa3857
TGFo <102477641+TGFo@users.noreply.github.com>
2024-03-22 11:20:09 +0200

    Update StateManager.cs

--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -23,22 +23,22 @@ public class StateManager : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            SetState(AIStates.Jumping);
-        }
-        else if (Input.GetKeyDown(KeyCode.A))
-        {
-            SetState(AIStates.Attacking);
-        }
-        else if (Input.GetKey(KeyCode.W))
-        {
-            SetState(AIStates.Walking);
-        }
-        else
-        {
-            SetState(AIStates.Idle);
-        }
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    SetState(AIStates.Jumping);
+        //}
+        //else if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    SetState(AIStates.Attacking);
+        //}
+        //else if (Input.GetKey(KeyCode.W))
+        //{
+        //    SetState(AIStates.Walking);
+        //}
+        //else
+        //{
+        //    SetState(AIStates.Idle);
+        //}
 
         PerformStateBehavior();
     }
@@ -53,14 +53,21 @@ public class StateManager : MonoBehaviour
     {
         switch (currentState)
         {
-            case AIStates.Idle:
-                break;
-            case AIStates.Walking:
-                break;
-            case AIStates.Jumping:
-                break;
-            case AIStates.Attacking:
-                break;
+            case AIStates.pursueFlag:
+
+            break;
+            case AIStates.avoidPlayer:
+
+            break;
+            case AIStates.attackPlayer:
+
+            break;
+            case AIStates.returnFlag:
+
+            break;
+            case AIStates.scorePoint:
+
+            break;
         }
     }
 
